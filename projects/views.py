8ae03796b8c97d890f4c3b6204cea3c7a9781e07
dannydjr <dannydelgadojr@gmail.com>
2025-01-05 14:15:07 +0800

    added dynamic data from DB using models

--- projects/views.py
+++ projects/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-projects_list = {
-    1: {'name': 'Website Redesign', 'description': 'Redesign the company website to improve user experience.'},
-    2: {'name': 'Mobile App Development', 'description': 'Develop a mobile application for our e-commerce platform.'},
-    3: {'name': 'SEO Optimization', 'description': 'Optimize the website for search engines to increase traffic.'},
-}
+from .models import Project, Tag, Review    
 
 # Create your views here.
 def projects(request):
-    msg = 'Hello, you are in the projects app'
-    number = 622
-    context = {
-        'msg': msg,
-        'number': number,
-        'projects': projects_list,
-    }
+    projectsObj = Project.objects.all()
+    context = {'projects': projectsObj,}
     return render(request, 'projects/projects.html', context)
 
-def project(request, pk):
+def project(request, id):
     try:
-        project = projects_list[int(pk)]
-    except KeyError:
-        project = None
-    return render(request, 'projects/single-project.html', {'pk': pk, 'project': project})
+        projectObj = Project.objects.get(id=id)
+        tags = projectObj.tags.all()
+    except Project.DoesNotExist:
+        projectObj = None
+    return render(request, 'projects/single-project.html', {'id': id, 'project': projectObj, 'tags': tags})
